048c4f7931270553532756bea38062765b10ce0d
Annika <annikahall@annikas-macbook-pro.local>
2019-02-03 14:44:23 -0500

    3.3 Completed: .last String/Optionals

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var userGuessLabel: UILabel!
     @IBOutlet weak var guessedLetterField: UITextField!
     @IBOutlet weak var guessedLetterButton: UIButton!
@@ -18,7 +19,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        guessedLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
     }
     
     func updateUIAfterGuess(){
@@ -27,6 +29,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        if let letterGuessed = guessedLetterField.text?.last{
+            guessedLetterField.text = "\(letterGuessed)"
+            guessedLetterButton.isEnabled = true
+        } else{
+            guessedLetterButton.isEnabled = false
+        }
         
     }
     @IBAction func doneKeyPressed(_ sender: UITextField) {
